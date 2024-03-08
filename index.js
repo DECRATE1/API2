2de28b471403a06a1b8c27a3f6bf981a3b1428b5
DECRATE1 <yandovin2005@mail.ru>
2024-03-08 19:33:12 +0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const taskRouter = require("./router/task")
-const { connect } = require("mongoose")
+const { connect, disconnect  } = require("mongoose")
 
 
 const app = express()
@@ -23,7 +23,7 @@ async function main(){
     }
 }
 
-main()
+main();
 
 
 process.on("SIGINT", async() => {
